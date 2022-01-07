b45d6ddca08b8a6eab35c63e00ac8fa4f9b683cd
eraste.mathieu@gmail.com <eraste.mathieu@gmail.com>
2022-01-07 12:08:43 +0100

    keys-input.js -> addEventListener keydown

--- js/keys-input.js
+++ js/keys-input.js
@@ -1,6 +1,52 @@
-let keyDirection = { x: 0, y: 0 };
+let keyDirection     = { x: 0, y: 0 };
+let lastKeyDirection = { x: 0, y: 0 }
+
+window.addEventListener('keydown', (e) => {
+    switch(e.key) {
+        case 'ArrowUp' :
+        case 'z' :
+        case 'Z' :
+            if(lastKeyDirection.y !== 0)
+                break;
+            
+            keyDirection = { x: 0, y: -1 };
+            break;
+
+        case 'ArrowDown' :
+        case 'x' :
+        case 'X' :
+            if(lastKeyDirection.y !== 0)
+            break;
+
+            keyDirection = { x: 0, y: 1 };
+            break;
+
+        case 'ArrowRight' :
+        case 'd' :
+        case 'D' :
+            if(lastKeyDirection.x !== 0)
+                break;
+            
+            keyDirection = { x: 1, y: 0 };
+            break;
+
+        case 'ArrowLeft' :
+        case 'q' :
+        case 'Q' :
+            if(lastKeyDirection.x !== 0)
+                break;
+            
+            keyDirection = { x: -1, y: 0 };
+            break;
+
+        default:
+            break;
+    }
+});
 
 function getKeyDirection() {
+    lastKeyDirection = keyDirection;
+
     return keyDirection;
 }
 
